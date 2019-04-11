8d3dec6b0aaa7ce0a5fc40d1b8a1dd6a31788dd5
Adam <ahanson845@gmail.com>
2019-04-10 19:59:23 -0500

    user controller and added venue name to venues table

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,35 @@
 class Api::UsersController < ApplicationController
-end
+
+  def index
+    @users = User.all
+    render 'index.json.jbuilder'
+  end
+
+  def create
+    @user = User.new(
+                    username: params[:username],
+                    email: params[:email],
+                    password: params[:password],
+                    password_confirmation: params[:password_confirmation]
+                    )
+    if @user.save
+      render json: {message: "Your profile was created"},status: :created
+    else
+      render json: {message: @user.errors.full_messages},status: :bad_request
+    end
+
+  def update 
+    user_id = params[:id]
+    @user = User.find(user_id)
+
+    @user.email = params[:email] || @user.email
+    @user.password = params[:password] || @user.password
+    @user.username = params[:username] || @user.username
+  end
+
+  def destroy
+    user_id = params[:id]
+    @user = User.find_by(user_id)
+    @user.destroy
+    render json:{message: "Successfully deleted user"}
+  end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190403005247) do
+ActiveRecord::Schema.define(version: 20190411002820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20190403005247) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "venue_name"
   end
 
   create_table "wishlists", force: :cascade do |t|
