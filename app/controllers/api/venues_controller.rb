c424282d04b5e6a19014584a58186ffc4bb88b2b
Adam <ahanson845@gmail.com>
2019-04-17 19:57:28 -0500

    completed venue controller

--- app/controllers/api/venues_controller.rb
+++ app/controllers/api/venues_controller.rb
@@ -1,2 +1,32 @@
 class Api::VenuesController < ApplicationController
+
+  def index
+    @venues = Venues.all 
+    render 'index.json.jbuilder'
+  end
+
+  def show 
+    @venue = Venue.find(params[:id])
+    render 'show.json.jbuilder'
+  end
+
+  def create
+    @venue = Venue.new(
+                      latitude: params[:latitude],
+                      longitude: params[:longitude],
+                      address: params[:address],
+                      venue_name: params[:venue_name] 
+                      )
+  if @venue.save
+    render json { message: "Venue has been added."}, status: :created
+  else
+    render json { message: "@venue.errors.full_messages"}, status: :bad_request
+  end
+
+  def destroy
+    @venue = Venue.find(params[:id])
+    @venue.destroy
+    render json: { message: "Venue has been removed"}
+  end
+  
 end
